94ed4746710b0049ba6cd2470fe171885823de50
Jonathan Rice <jottorice@gmail.com>
2017-12-29 18:26:31 -0600

    Remove unused factories.

--- www/js/services.js
+++ www/js/services.js
@@ -3,6 +3,7 @@
 angular.module('inventory.services', ['ngResource'])
     .constant("baseURL", "https://whispering-mountain-55762.herokuapp.com/")
 
+    // Factory connected to remote MongoDB database
     .factory('itemFactory', ['$resource', 'baseURL', function($resource, baseURL) {
 
         return $resource(baseURL + "items/:id", null, {
@@ -11,66 +12,4 @@ angular.module('inventory.services', ['ngResource'])
             }
         });
 
-    }])
-
-    .factory('menuFactory', ['$resource', 'baseURL', function($resource, baseURL) {
-
-        return $resource(baseURL + "dishes/:id", null, {
-            'update': {
-                method: 'PUT'
-            }
-        });
-
-    }])
-
-    .factory('promotionFactory', ['$resource', 'baseURL', function($resource, baseURL) {
-        return $resource(baseURL + "promotions/:id");
-
-    }])
-
-    .factory('corporateFactory', ['$resource', 'baseURL', function($resource, baseURL) {
-
-
-        return $resource(baseURL + "leadership/:id");
-
-    }])
-
-    .factory('feedbackFactory', ['$resource', 'baseURL', function($resource, baseURL) {
-
-
-        return $resource(baseURL + "feedback/:id");
-
-    }])
-
-    .factory('favoriteFactory', ['$resource', 'baseURL', '$localStorage', function($resource, baseURL, $localStorage) {
-        var favFac = {};
-        var favorites = $localStorage.getObject('favorites', '[]');
-
-        favFac.addToFavorites = function(index) {
-            for (var i = 0; i < favorites.length; i++) {
-                if (favorites[i].id == index)
-                    return;
-            }
-            favorites.push({
-                id: index
-            });
-            $localStorage.storeObject('favorites', favorites);
-        };
-
-        favFac.deleteFromFavorites = function(index) {
-            for (var i = 0; i < favorites.length; i++) {
-                if (favorites[i].id == index) {
-                    favorites.splice(i, 1);
-                }
-            }
-            $localStorage.storeObject('favorites', favorites);
-        }
-
-        favFac.getFavorites = function() {
-            return favorites;
-        };
-
-        return favFac;
-    }])
-
-;
+    }]);
