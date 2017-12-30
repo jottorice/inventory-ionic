51074114a204ec6b0fed4c96eac3edb8c78d8320
Jonathan Rice <jottorice@gmail.com>
2017-12-29 19:21:44 -0600

    Got pull-down refresh to work

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,7 @@ angular.module('inventory.controllers', [])
     })
 
 
-    .controller('IndexController', ['$scope', 'items', 'baseURL', function($scope, items, baseURL) {
+    .controller('IndexController', ['$scope', 'items', 'baseURL', 'itemFactory', function($scope, items, baseURL, itemFactory) {
 
         $scope.baseURL = baseURL;
         $scope.message = "Loading ...";
@@ -16,4 +16,29 @@ angular.module('inventory.controllers', [])
         console.log("IndexController resolved items:");
         console.log(items);
         
+        $scope.doRefresh = function() {
+            console.log("In refresh");
+            
+            var items = itemFactory.query()
+                .$promise.then(
+                    function (response) {
+                        var items = response;
+                        console.log("Refreshed items:");
+                        console.log(items);
+                        $scope.items = items;
+                        $scope.showItems = true;
+                        //Stop the ion-refresher from spinning
+                        $scope.$broadcast('scroll.refreshComplete');
+                    },
+                    function (response) {
+                        $scope.message = "Error when refreshing: " + response.status + " " + response.statusText;
+                         //Stop the ion-refresher from spinning
+                        $scope.$broadcast('scroll.refreshComplete');
+                   }
+                )
+
+
+
+        }
+
     }]);
